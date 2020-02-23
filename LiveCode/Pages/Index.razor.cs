07dfb7a2f7dcce86b72b3b7dd151032bdcca5459
lark <lilinghui@gmail.com>
2020-02-23 16:22:20 +0800

    demo init

---
+++ LiveCode/Pages/Index.razor.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace LiveCode.Pages
+{
+    public class IndexModel : ComponentBase
+    {
+        public string Output = "";
+        const string DefaultCode = @"
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(""Hello World"");
+    }
+}";
+
+        [Inject] private HttpClient Client { get; set; }
+        [Inject] private Monaco Monaco { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            Compiler.InitializeMetadataReferences(Client);
+            return base.OnInitializedAsync();
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+            if (firstRender)
+            {
+                Monaco.Initialize("container", DefaultCode, "csharp");
+            }
+        }
+
+        public void Run()
+        {
+            Compiler.WhenReady(RunInternal);
+        }
+
+        async Task RunInternal()
+        {
+            Output = "";
+
+            Console.WriteLine("Compiling and Running code");
+            var sw = Stopwatch.StartNew();
+
+            var currentOut = Console.Out;
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Exception exception = null;
+            try
+            {
+                var (success, asm) = Compiler.LoadSource(Monaco.GetCode("container"));
+                if (success)
+                {
+                    var entry = asm.EntryPoint;
+                    if (entry.Name == "<Main>") // sync wrapper over async Task Main
+                    {
+                        entry = entry.DeclaringType.GetMethod("Main", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static); // reflect for the async Task Main
+                    }
+                    var hasArgs = entry.GetParameters().Length > 0;
+                    var result = entry.Invoke(null, hasArgs ? new object[] { new string[0] } : null);
+                    if (result is Task t)
+                    {
+                        await t;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            Output = writer.ToString();
+            if (exception != null)
+            {
+                Output += "\r\n" + exception.ToString();
+            }
+            Console.SetOut(currentOut);
+
+            sw.Stop();
+            Console.WriteLine("Done in " + sw.ElapsedMilliseconds + "ms");
+
+            StateHasChanged();
+        }
+    }
+}
